7580bc65dde8c274a08d6926612d8eb1ac90e262
Michael Mandel <mim@mr-pc.org>
2014-12-17 11:43:20 -0500

    Formatting and comments

--- L02exampleParabaloid.m
+++ L02exampleParabaloid.m
@@ -2,8 +2,16 @@ function L02exampleParabaloid()
 
 % Plot a 3D parabaloid with iso-contour lines
 
+% Meaningful parameters
 a = 2; b = 1; xc = -1; yc = -1; th = pi/4;
-A = a^2*sin(th)^2 + b^2*cos(th)^2; B = 2*(b^2-a^2)*sin(th)*cos(th); C = a^2*cos(th)^2 + b^2*sin(th)^2; D = -2*A*xc - B*yc; E = -B*xc-2*C*yc; F = A*xc^2+B*xc*yc+C*yc^2-a^2*b^2;
+
+% Derived coefficients
+A = a^2*sin(th)^2 + b^2*cos(th)^2; 
+B = 2*(b^2-a^2)*sin(th)*cos(th); 
+C = a^2*cos(th)^2 + b^2*sin(th)^2; 
+D = -2*A*xc - B*yc; 
+E = -B*xc-2*C*yc; 
+F = A*xc^2+B*xc*yc+C*yc^2-a^2*b^2;
 
 [y x] = meshgrid(linspace(-3,3,31), linspace(-3,3,31));
 z = A*x.^2 + B*x.*y + C*y.^2 + D*x + E*y + F;
